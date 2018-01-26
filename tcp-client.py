990337b85d6e9e535eedd4ccdf87ea8bd466ec72
Obzidian <jadelangel87@gmail.com>
2018-01-26 11:25:47 -0600

    Changed host to test tcp server

--- tcp-client.py
+++ tcp-client.py
@@ -3,13 +3,13 @@
 import socket
 
 # Set target info
-host = "www.linuxacademy.com"
-port = 80
+host = "127.0.0.1"
+port = 9999
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((host,port))
-client.send("GET / HTTP/1.1\r\nHost: linuxacademy.com\r\n\r\n")
+client.send("ABCDEF\r\n")
 
 reply = client.recv(4096)
 
